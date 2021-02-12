7d96870de1cb0d98bb6f1b140d6ff36235c41376
John3den <64809077+John3den@users.noreply.github.com>
2021-02-12 19:46:16 +0700

    Add files via upload

---
+++ sem0902.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define ll long long
+#include <stdio.h>
+int x,y;
+int main() {
+	scanf("%d%d", &x, &y);
+	if ((ll)(x)+(ll)(y) == x+y)
+		printf("x+y=%d\n", x + y);
+	else 
+		printf("numbers are too big, Mason!\n");
+	printf("x*y=%d\n", x * y);
+	if (y != 0)
+	{
+		printf("x/y=%d\n", x / y);
+		printf("x%%y=%d\n", x % y);
+	}
+	else
+	{
+		printf("Division by zero!\n");
+	}
+	printf("x-y=%d\n", x - y);
+	printf("x>=y=%d\n", x >= y);
+	printf("x<=y=%d\n", x <= y);
+	printf("x>y=%d\n", x > y);
+	printf("x<y=%d\n", x < y);
+	printf("x==y=%d\n", x == y);
+	printf("x!=y=%d\n", x != y);
+	return 0;
+}
