7cdd23c54854049d54864a7aa087143226c34c83
John3den <john_henry_eden@yahoo.com>
2021-03-02 13:50:54 +0700

    Update task3.c

--- lab2602/task3.c
+++ lab2602/task3.c
@@ -4,21 +4,23 @@
 
 void display(int arr[]) {
 	printf("Prime numbers less than %i: ", N);
-	for (int i = 2; i < N; i++) {
-		if (arr[i] == 1)
-			printf("%i ", i);
+	for (int i = 2; i < N; i++)
+	{
+		if (arr[i] == 1) printf("%i ", i);
 	}
 }
 
 void fillArray(int arr[]) {
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
+	{
 		arr[i] = 1;
 	}
 }
 
 void eraseMultiplicatives(int arr[], int x) {
 	int p = x + x;
-	while (p < N) {
+	while (p < N) 
+	{
 		arr[p] = -1;
 		p += x;
 	}
@@ -26,9 +28,9 @@ void eraseMultiplicatives(int arr[], int x) {
 
 
 void EratosthenesSieve(int arr[]) {
-	for (int i = 2; i < N; i++) {
-		if (arr[i] == 1)
-			eraseMultiplicatives(arr, i);
+	for (int i = 2; i < N; i++)
+	{
+		if (arr[i] == 1) eraseMultiplicatives(arr, i);
 	}
 }
 
