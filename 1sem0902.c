f5eb7f662289298efcb4c007db85546ac5268cb2
John3den <64809077+John3den@users.noreply.github.com>
2021-03-22 17:11:35 +0700

    Update 1sem0902.c

--- 1sem0902.c
+++ 1sem0902.c
@@ -3,28 +3,6 @@
 #include <stdio.h>
 int x,y;
 int main() {
-	scanf("%d%d", &x, &y);
-	if ((ll)(x)+(ll)(y) == x+y)
-		printf("x+y=%d\n", x + y);
-	else 
-		printf("numbers are too big, Mason!\n");
-	printf("x*y=%d\n", x * y);
-	if (y != 0)
-	{
-		printf("x/y=%d\n", x / y);
-		printf("x%%y=%d\n", x % y);
-	}
-	else
-	{
-		printf("Division by zero!\n");
-	}
-	printf("x-y=%d\n", x - y);
-	printf("x>=y=%d\n", x >= y);
-	printf("x<=y=%d\n", x <= y);
-	printf("x>y=%d\n", x > y);
-	printf("x<y=%d\n", x < y);
-	printf("x==y=%d\n", x == y);
-	printf("x!=y=%d\n", x != y);
-	printf("x's address is %i\n",&x);
+	// i like socks
 	return 0;
 }
